7e058c11117ce147f358a49be15354c616db3bfd
Tom Warne <twarne@gmail.com>
2022-05-20 17:14:29 -0700

    Initial commit

---
+++ src/main/java/com/kickthedrawer/turtles/graphinitializer/GraphInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.kickthedrawer.turtles.graphinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphInitializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kickthedrawer/turtles/graphinitializer/GraphInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickthedrawer.turtles.graphinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
